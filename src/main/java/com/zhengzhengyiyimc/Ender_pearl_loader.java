631154cf42f3651d82075633ae31129502ea6aef
zhengzhengyiyi <zhengzhichen44@163.com>
2025-06-25 21:05:16 -0400

    init submit

---
+++ src/main/java/com/zhengzhengyiyimc/Ender_pearl_loader.java
@@ -0,0 +1,16 @@
+package com.zhengzhengyiyimc;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Ender_pearl_loader implements ModInitializer {
+	public static final String MOD_ID = "ender_pearl_loader";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
